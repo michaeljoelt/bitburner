0c1ba74d841e997902f13618e30a260f4e028fac
michaeljoelt <michaeljoelt@gmail.com>
2022-01-06 15:36:30 -0800

    updating files

---
+++ compare.js
@@ -0,0 +1,45 @@
+/** @param {NS} ns **/
+/*
+Name: autoHack.js 
+Last updated: 2022-01-02-1741
+*/
+export async function main(ns) {
+    let moneyThresh = 1000000;
+    let vers = "v1";
+    let host = ns.getHostname();
+    let target = host;
+    if (ns.args.length > 0) {
+        target = ns.args[0];
+    }
+    ns.print(vers + "|" + host + "|" + Date.now() + ": target: " + target);
+ 
+    let myMoney = ns.getServerMoneyAvailable("home");
+    let serverThresh = ns.getServerMaxMoney(target) * 0.75;
+    if(myMoney < moneyThresh)
+    {
+        moneyThresh = myMoney;
+    }
+    else if(myMoney > serverThresh)
+    {
+        moneyThresh = serverThresh;
+    }
+    ns.print(vers + "|" + host + "|" + Date.now() + ": moneyTresh: " + moneyThresh);
+    let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+    ns.print(vers + "|" + host + "|" + Date.now() + ": securityThresh: " + securityThresh);
+ 
+    while (true) {
+        ns.print(vers + "|" + host + "|" + Date.now() + ": target: " + target + "; $" + ns.getServerMoneyAvailable(target) + " ; security: " + ns.getServerMinSecurityLevel(target));
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            ns.print(vers + "|" + host + "|" + Date.now() + ": security level (" + ns.getServerSecurityLevel(target) + ") > securityThresh(" + securityThresh + ")");
+            ns.print(vers + "|" + host + "|" + Date.now() + ": running weaken");
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            ns.print(vers + "|" + host + "|" + Date.now() + ": money available (" + ns.getServerMoneyAvailable(target) + ") < moneyThresh(" + moneyThresh + ")");
+            ns.print(vers + "|" + host + "|" + Date.now() + ": grow " + target);
+            await ns.grow(target);
+        } else {
+            ns.print(vers + "|" + host + "|" + Date.now() + ": hacking " + target + "($" + ns.getServerMoneyAvailable(target) + ")");
+            await ns.hack(target);
+        }
+    }
+}
--- recursiveScan.js
+++ recursiveScan.js
@@ -1,22 +1,94 @@
 /** @param {NS} ns **/
-/* 
-Last updated: 1/2/2022 2:15
+/*
+Name: recursiveScan.js 
+Last updated: 2022-01-03-0235
+
+args -> 
+all = returns all servers (also if you put no argument)
+rootTrue = returns servers with root access
+rootFalse = returns servers without root access
+ports0 = returns servers with port 0 (change to 1-5 for the others)
+hackableLevelTrue = returns servers with lower hack level than players current level
+hackableLevelFalse = returns servers with higher hack level than players current level
+
+to add later?: hackableTrue; hackable false - based on everything (ports, level, root)
+
 */
-/*recursive code template credit: milonti on reddit https://www.reddit.com/r/Bitburner/comments/9uuh3u/recursive_variables/ */
+/*recursive code credit: milonti on reddit https://www.reddit.com/r/Bitburner/comments/9uuh3u/recursive_variables/ */
+//let arg = "all";
 export async function main(ns) {
+	let arg = ns.args[0]; //custom
+	ns.tprint("returning based on arg: " + arg + "(to return all servers if blank)"); //custom
 	let scanned = [];
-	function recursive(host) {
+	const uniqueSet = new Set(); //custom
+	const returnArray = [];
+	async function recursive(host) {
 		scanned.push(host);
-		let newScan = ns.scan(host);
+		let newScan = ns.scan(host); //custom
+
 		for (let x = 0; x < newScan.length; x++) {
+
 			if (scanned.includes(newScan[x])) continue;
 			//Insert recursing code below
-			if (ns.getServerMaxMoney(host) > 0) {
-				ns.tprint("server: " + host + " ; maxMoney: " + ns.getServerMaxMoney(host));
+			if (arg == "rootFalse" && ns.hasRootAccess(host) == false) {
+				ns.tprint("assumedArg" + "rootFalse ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				//returnArray.push(server);
+				uniqueSet.add(host);
 			}
+			else if (arg == "rootTrue" && ns.hasRootAccess(host) == true) {
+				ns.tprint("assumedArg" + "rootTrue ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else if (arg == "ports0" && ns.getServerNumPortsRequired(host) == 0) {
+				ns.tprint("assumedArg" + "ports0 ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else if (arg == "ports1" && ns.getServerNumPortsRequired(host) == 1) {
+				ns.tprint("assumedArg" + "ports1 ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else if (arg == "ports2" && ns.getServerNumPortsRequired(host) == 2) {
+				ns.tprint("assumedArg" + "ports2 ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else if (arg == "ports3" && ns.getServerNumPortsRequired(host) == 3) {
+				ns.tprint("assumedArg" + "ports3 ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else if (arg == "ports4" && ns.getServerNumPortsRequired(host) == 4) {
+				ns.tprint("assumedArg" + "ports4 ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else if (arg == "ports5" && ns.getServerNumPortsRequired(host) == 5) {
+				ns.tprint("assumedArg" + "ports5 ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+			else {
+				ns.tprint("assumedArg" + "none or all ; " + "server:" + host + " ; maxMoney:" + ns.getServerMaxMoney(host) + " ; maxRam:" + ns.getServerMaxRam(host) + " ; portsRequired:" + ns.getServerNumPortsRequired(host) + " ; requiredHackingLvl:" + ns.getServerRequiredHackingLevel(host) + " ; rootAccess:" + ns.hasRootAccess(host));
+				uniqueSet.add(host);
+			}
+
+
+
+			// if (ns.getServerMaxMoney(host) > 0) {
+			// 	//ns.tprint("server: " + host + " ; maxMoney: " + ns.getServerMaxMoney(host));
+			// 	uniqueSet.add(host);
+			// }
 			//Insert recursing code above
-			recursive(newScan[x])
+			await recursive(newScan[x]);
 		}
 	}
-	recursive('home');
+	await recursive('home');
+
+	/* also custom code */
+	uniqueSet.forEach(function (server) {
+	//ns.tprint(server);
+		returnArray.push(server);
+	//return based on argument
+	//rootTrue,rootFalse,ports0,hackableTrue,hackableFalse
+
+	});
+	ns.tprint("TO RETURN ARRAY: " + returnArray);
+	return returnArray;
+	/* custom code end */
 }
